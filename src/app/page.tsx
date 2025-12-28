6024494c81afb6c1f140fa763ce50215e8a1452f
Dilum srimal wimalaweera <srimalwimalaweera@gmail.com>
2025-12-28 12:01:36 +0000

    මේ header එක වටේට margin දාපන්.  (_for element <Header>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,9 @@ import { Footer } from "@/components/layout/footer";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <Header />
+      <div className="m-4">
+        <Header />
+      </div>
       <main className="flex-1">
         <Hero />
         <Services />
