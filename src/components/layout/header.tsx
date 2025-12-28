b4f161150966728c64dbadb1abde41870256fee3
Dilum srimal wimalaweera <srimalwimalaweera@gmail.com>
2025-12-28 11:55:44 +0000

    header's logo. move little bit right.

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -15,7 +15,7 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-7xl items-center">
-        <Link href="#home" className="mr-6 flex items-center space-x-2">
+        <Link href="#home" className="mr-4 flex items-center space-x-2">
           <Logo className="h-8 w-24" />
         </Link>
         <nav className="hidden items-center gap-6 text-sm font-medium md:flex">
